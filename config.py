01c20effcd97ab932653cfea90671188f1c74afd
Kamoliddinov Asilbek | Async Boy <asilbek.async@gmail.com>
2026-08-12 21:41:26 +0500

    chore: initial core setup and sqlalchemy patch

--- config.py
+++ config.py
@@ -13,9 +13,27 @@ MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost")
 MYSQL_PORT = int(os.getenv("MYSQL_PORT", "3306"))
 MYSQL_USER = os.getenv("MYSQL_USER", "root")
 MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "")
-MYSQL_DB = os.getenv("MYSQL_DB", "aiogram_start")
+MYSQL_DB = os.getenv("MYSQL_DB", "nikox")
 
 DATABASE_URL = (
     f"mysql+asyncmy://{MYSQL_USER}:{MYSQL_PASSWORD}@"
     f"{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
 )
+
+# Admin
+admin_ids_raw = os.getenv("ADMIN_IDS", "")
+ADMIN_IDS = [int(x.strip()) for x in admin_ids_raw.split(",")] if admin_ids_raw else []
+
+# App settings
+WELCOME_BONUS_UZS = int(os.getenv("WELCOME_BONUS_UZS", "5000"))
+REFERRAL_BONUS_UZS = int(os.getenv("REFERRAL_BONUS_UZS", "3000"))
+REQUEST_FEE_UZS = int(os.getenv("REQUEST_FEE_UZS", "1000"))
+PRIVATE_CONTACT_FEE_UZS = int(os.getenv("PRIVATE_CONTACT_FEE_UZS", "1000"))
+SEARCH_ACCESS_FEE_UZS = int(os.getenv("SEARCH_ACCESS_FEE_UZS", "6000"))
+HIDDEN_PROFILE_ACCESS_FEE_UZS = int(os.getenv("HIDDEN_PROFILE_ACCESS_FEE_UZS", "6000"))
+CHAT_LIFETIME_DAYS = int(os.getenv("CHAT_LIFETIME_DAYS", "7"))
+
+VIP_1_DAY_UZS = int(os.getenv("VIP_1_DAY_UZS", "249000"))
+VIP_1_WEEK_UZS = int(os.getenv("VIP_1_WEEK_UZS", "449000"))
+VIP_1_MONTH_UZS = int(os.getenv("VIP_1_MONTH_UZS", "990000"))
+
--- database/base.py
+++ database/base.py
@@ -1,3 +1,9 @@
+import sqlalchemy.util.typing
+def fixed_make_union(*types):
+    import typing
+    return typing.Union[types]
+sqlalchemy.util.typing.make_union_type = fixed_make_union
+
 from sqlalchemy.orm import DeclarativeBase
 
 
